350dd97fb675823c58b2999e9c731acb576cc6bb
Andrew Gillard <andrew@lorddeath.net>
2016-06-11 23:37:04 +0100

    Initial working version

---
+++ www/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>Stay Open! Keep Your Browser Windows Open!</title>
+    <link rel="canonical" href="http://stay.o.pen.wf/">
+    <link rev="canonical" href="http://o.pen.wf/">
+    <style type="text/css">
+        body
+        {
+            padding-top: 50px;
+            font-family: Verdana, Arial, sans-serif;
+        }
+
+        header h1
+        {
+            text-align: center;
+            font-size: 1em;
+            font-weight: normal;
+            color: #707070;
+        }
+        header h1 a {
+            text-decoration: none;
+            color: inherit;
+        }
+        header h1 span {
+            font-size: 2em;
+            font-weight: bold;
+            color: #000000;
+        }
+
+        article
+        {
+            width: 80%;
+            line-height: 40px;
+            font-weight: normal;
+            font-size: 1.1em;
+            text-align: center;
+        }
+    </style>
+    <script>
+        window.onbeforeunload = function() {
+            return "Are you sure you wish to close your browser window?";
+        }
+    </script>
+</head>
+<body>
+<header>
+    <h1><a href="/"><span>stay.o.pen</span>.wf</a></h1>
+</header>
+<article>
+    <p>Keep accidentally closing your browser windows? Keep this page open in a tab (pin it!) in each window to force your browser to confirm any attempts to close a window!</p>
+</article>
+<footer>
+    <p>This page was heavily inspired by <a href="http://www.maki-chan.de/preventclose.htm">"Prevent browser close tab"</a>.</p>
+</footer>
+</body>
+</html>
